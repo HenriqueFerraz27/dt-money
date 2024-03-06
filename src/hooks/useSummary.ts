3b64b43fb246b9c0d5c0862cc430836d299b9d68
Henrique Ferraz <henriqueferrazsilva27@gmail.com>
2024-03-05 23:10:56 -0800

    feat(hook): apply useMemo

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,26 +1,31 @@
+import { useMemo } from 'react'
 import { useTransactions } from './useTransactions'
 
 export const useSummary = () => {
   const { transactions } = useTransactions()
 
-  return transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.segment === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      }
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.segment === 'income') {
+          acc.income += transaction.price
+          acc.total += transaction.price
+        }
 
-      if (transaction.segment === 'outcome') {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
+        if (transaction.segment === 'outcome') {
+          acc.outcome += transaction.price
+          acc.total -= transaction.price
+        }
 
-      return acc
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+        return acc
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      }
+    )
+  }, [transactions])
+
+  return summary
 }
